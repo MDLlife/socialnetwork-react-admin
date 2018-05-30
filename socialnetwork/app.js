2d1c52ec0ccd3518bb3adf4dc5399849eb200d3a
Thomas Modeneis <thomas.modeneis@gmail.com>
2018-05-30 11:22:24 +0200

    #1 - init project

--- socialnetwork/app.js
+++ socialnetwork/app.js
@@ -10,8 +10,8 @@ import { UserList, UserEdit, UserCreate, UserIcon } from './user';
 import Dashboard from './dashboard';
 import Settings from './settings';
 
-// const restClient = comentarismoAPI('https://api.mdl.live');
-const restClient = comentarismoAPI('http://localhost:3010/v1');
+const restClient = comentarismoAPI('https://api.mdl.live/v1');
+// const restClient = comentarismoAPI('http://localhost:3010/v1');
 render(
     <Admin dashboard={Dashboard} settings={Settings} restClient={restClient}>
         <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} remove={Delete} icon={UserIcon} />
